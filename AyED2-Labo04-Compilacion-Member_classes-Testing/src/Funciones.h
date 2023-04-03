f24320db107ca5bf0d62c126251b27d760fd650e
AxelBelbrun <128548216+AxelBelbrun@users.noreply.github.com>
2023-04-03 20:50:12 -0300

    Ejercicios del taller 2, dividir archivos, incompleto

---
+++ AyED2-Labo04-Compilacion-Member_classes-Testing/src/Funciones.h
@@ -0,0 +1,10 @@
+//
+// Created by clinux01 on 03/04/23.
+//
+
+#ifndef SOLUCION_FUNCIONES_H
+#define SOLUCION_FUNCIONES_H
+#include "Meses.h"
+bool esBisiesto(Anio anio);
+int diasEnMes(int anio, int mes);
+#endif //SOLUCION_FUNCIONES_H
