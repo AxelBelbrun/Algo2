8ed247de2afc922dac75172431fb0aa90559fe2b
Axel Belbrun <128548216+AxelBelbrun@users.noreply.github.com>
2023-05-24 18:55:51 -0300

    Add files via upload

---
+++ entregable4/ricardi_tp2.R
@@ -0,0 +1,5 @@
+#2 b) no da resultado ya que no se encuentra en el rango que impusimos inicialmente
+#3) la funcion es sort
+#4) si, generan la misma matriz
+#5) salta error porq para multiplicar matrices seria a%*%b 
+ te pone el vector que antes estaba en columna, en fila ya que se traspuso. 
